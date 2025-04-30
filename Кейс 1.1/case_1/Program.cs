019e7720d06553c372e9f23d70380eef2f49a6f8
Danilkaaa323 <danilkaaa323@gmail.com>
2025-04-30 19:28:01 +0300

    Add files via upload

---
+++ Кейс 1.1/case_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace case_1
+{
+    internal static class Program
+    {
+        public static List<string> ShowOrder { get; set; } = new List<string>();
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            List<double> maxResources = new List<double>();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Settings(new List<double>()));
+        }
+    }
+}
